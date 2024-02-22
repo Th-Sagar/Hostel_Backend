591e9d3b057c79495662f2191279aa6af7936ec9
Th-Sagar <sagarthapa98122@gmail.com>
2024-02-22 21:48:28 +0545

    feat:swagger

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import authRouter from "./router/auth-router.js";
 import updateRouter from "./router/update-router.js";
 import hostelDetailsRouter from "./router/hostelDetails-router.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from './swagger-output.json' assert { type: 'json' };
+
 const app = express();
 const port = 5000;
 
@@ -12,10 +15,11 @@ app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/user", updateRouter);
 app.use("/api/hostel", hostelDetailsRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 connectDb().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 });
-
